4b9973c5feb15810951b8fefe81a64f2f4783efa
hsonthach <son.thach011011@gmail.com>
2025-07-04 10:56:42 -0700

    Refactor RestClientApplication to manage books instead of contacts. Updated server URL and modified methods to add, retrieve, update, and delete books using the Book class. LAB 2

--- lab2/part2/BookClient/src/main/java/contacts/RestClientApplication.java
+++ lab2/part2/BookClient/src/main/java/contacts/RestClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+import books.Book;
 
 @SpringBootApplication
 public class RestClientApplication implements CommandLineRunner {
@@ -19,36 +20,41 @@ public class RestClientApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String serverUrl = "http://localhost:8080/contacts";
+		String serverUrl = "http://localhost:8080/api/books";
 
-		// add Frank
-		restTemplate.postForLocation(serverUrl, new Contact("Frank","Browns", "fbrowns@acme.com",
-				"0639332163"));
-		// add John
-		restTemplate.postForLocation(serverUrl, new Contact("John","Doe", "jdoe@acme.com",
-				"6739127563"));
-		// get frank
-		Contact contact= restTemplate.getForObject(serverUrl+"/{firstName}", Contact.class, "Frank");
-		System.out.println("----------- get John-----------------------");
-		System.out.println(contact.getFirstName()+" "+contact.getLastName());
-        // get all
-		Contacts contacts= restTemplate.getForObject(serverUrl, Contacts.class);
-		System.out.println("----------- get all contacts-----------------------");
-		System.out.println(contacts);
+		// add Book 1
+		restTemplate.postForLocation(serverUrl,
+				new Book("978-0-7475-3269-9", "J.K. Rowling", "Harry Potter and the Philosopher's Stone", 29.99));
+		// add Book 2
+		restTemplate.postForLocation(serverUrl,
+				new Book("978-0-7475-3849-3", "J.K. Rowling", "Harry Potter and the Chamber of Secrets", 32.99));
 
-		// delete John
-		restTemplate.delete(serverUrl+"/{firstName}", "John");
+		// get book by ISBN
+		Book book = restTemplate.getForObject(serverUrl + "/{isbn}", Book.class, "978-0-7475-3269-9");
+		System.out.println("----------- get Book by ISBN-----------------------");
+		System.out.println(book.getTitle() + " by " + book.getAuthor());
 
-		// update frank
-		contact.setEmail("franky@gmail.com");
-		restTemplate.put(serverUrl+"/{firstName}", contact, contact.getFirstName());
+		// get all books
+		Book[] books = restTemplate.getForObject(serverUrl, Book[].class);
+		System.out.println("----------- get all books-----------------------");
+		for (Book b : books) {
+			System.out.println(b);
+		}
 
-		// get all
-		contacts= restTemplate.getForObject(serverUrl, Contacts.class);
-		System.out.println("----------- get all contacts-----------------------");
-		System.out.println(contacts);
+		// delete book by ISBN
+		restTemplate.delete(serverUrl + "/{isbn}", "978-0-7475-3849-3");
+
+		// update book
+		book.setPrice(34.99);
+		restTemplate.put(serverUrl, book);
+
+		// get all books after updates
+		books = restTemplate.getForObject(serverUrl, Book[].class);
+		System.out.println("----------- get all books after updates-----------------------");
+		for (Book b : books) {
+			System.out.println(b);
+		}
 	}
-
 
 	@Bean
 	RestOperations restTemplate() {
